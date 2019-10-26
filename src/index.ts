63381031e015f2a355a74ae7e353e1d9f1cbce2b
lolimay <lolimay@lolimay.cn>
2019-10-26 22:30:20 +0800

    Add global clock

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
 import  * as dat from 'dat.gui';
 import { FCFS } from './algorithms/FCFS';
 import { PCB } from './PCB';
+import { print } from './utils';
 import './index.css';
 import 'xterm/css/xterm.css';
 
 var store = {
+    currentTime: 0,
+    clockSpeed: 1,
     fcfsControls: {
-        clockSpeed: 1,
     },
 };
+
+const gui = new dat.GUI();
+gui.add(store, 'clockSpeed', 0.5, 10);
+
+// Global Clock
+const clockElement = document.querySelector('#clock');
+setTimeout(function timer() {
+    store.currentTime++;
+    if (clockElement !== null && clockElement.innerHTML !== null) {
+        clockElement.innerHTML = `当前时间: ${ store.currentTime.toString() }`;
+    }
+    setTimeout(timer, 1/store.clockSpeed * 1000)
+}, 1/store.clockSpeed * 1000)
+
+// PCFS
 const pcbs: Array<PCB> = [];
 
 for (let i=0; i<5; i++) {
@@ -16,6 +33,3 @@ for (let i=0; i<5; i++) {
 }
 
 FCFS(...pcbs);
-
-const gui = new dat.GUI();
-gui.add(store.fcfsControls, 'clockSpeed', -5, 5);
